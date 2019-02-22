4f507cecce0246ce907044c630fa19eb720e4d55
Chaitanya Gangavarapu <chaitanya.gangavarapu@gmail.com>
2019-02-21 16:42:36 -0800

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Chai Gangavarapu on 2/21/19.
+//  Copyright © 2019 Chaitanya Gangavarapu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
